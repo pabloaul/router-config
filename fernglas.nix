bb627029212222970cb7ed56f3ef2b58cb9e712c
Nojus <dn42@nojus.org>
2025-06-09 00:47:14 +0200

    fernglas: connect with bird via bmp

--- fernglas.nix
+++ fernglas.nix
@@ -12,16 +12,30 @@ in
     settings = {
       api.bind = "[::1]:3000";
       collectors = {
-        # my_bmp_collector = {
-        #   collector_type = "Bmp";
-        #   bind = "[::]:${toString bmpPort}";
-        #   peers = {
-        #     "192.0.2.1" = {};
-        #   };
-        # };
+        bmp_central = {
+          collector_type = "Bmp";
+          bind = "[::1]:11019";
+          peers = {
+            "fd42:deca:de::1" = {};
+          };
+        };
       };
     };
   };
+
+  services.bird.config = ''
+    protocol bmp {
+      system name "central";
+      system description "pleiades_bmp";
+      local address fd42:deca:de::1;
+      station address ip ::1 port 11019;
+
+      monitoring rib in pre_policy on;
+      monitoring rib in post_policy on;
+
+      tx buffer limit 96;
+    }
+  '';
 
   services.nginx = {
       enable = true;
