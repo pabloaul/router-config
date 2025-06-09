316e63a85c42f099926564572860c7d81e382341
Nojus <dn42@nojus.org>
2025-06-09 17:28:25 +0200

    net: explicitly disable rp_filter

--- networking.nix
+++ networking.nix
@@ -7,6 +7,8 @@
 
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = true;
+    "net.ipv4.conf.all.rp_filter" = 0;
+    "net.ipv4.conf.default.rp_filter" = 0;
     "net.ipv6.conf.all.forwarding" = true;
   };
 
@@ -16,6 +18,7 @@
     hostName = "central";
     domain = "net.nojus.org";
     firewall.enable = true;
+    firewall.checkReversePath = false;
     useNetworkd = true;
    };
 
